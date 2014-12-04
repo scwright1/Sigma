71a0b2ff26f925f3e7fb92d47d77ac94b3bbf32f
Stephen Wright <ste.c.wr@gmail.com>
2014-12-04 15:01:41 +0000

    #1 - continuing with login

---
+++ app/controllers/auth/login.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+var Login = Ember.ObjectController.extend({
+    variable: 'LoginController'
+});
+
+export default Login;
---
+++ app/views/auth/login.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+var Login = Ember.View.extend({
+    variable: 'LoginView'
+});
+
+export default Login;
