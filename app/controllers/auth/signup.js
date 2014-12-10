9b20d27bb03df939a318fdc7e07f8122a675215a
Stephen Wright <ste.c.wr@gmail.com>
2014-12-10 15:05:17 +0000

    #3 - added password validation css to signup form

--- app/controllers/auth/signup.js
+++ app/controllers/auth/signup.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
+import ic from 'ic-ajax';
 
 var Signup = Ember.ObjectController.extend({
+    content: {},
+    actions: {
+        signup: function() {
+            var self = this;
+
+        }
+    }
 });
 
 export default Signup;
--- app/views/auth/signup.js
+++ app/views/auth/signup.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 var Signup = Ember.View.extend({
-
+    validationFeedback: function() {
+        if(this.get('controller.confirm') || this.get('controller.password')) {
+            //color the border of the confirm element if the passwords don't match
+            if(this.get('controller.confirm') !== this.get('controller.password')) {
+                console.log('no');
+                this.$('section > div > section > form > .confirm').addClass('invalid');
+            } else {
+                this.$('section > div > section > form > .confirm').removeClass('invalid');
+            }
+        }
+    }.observes('controller.password', 'controller.confirm').on('didInsertElement')
 });
 
 export default Signup;
